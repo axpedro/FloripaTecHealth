4f3c4324f9dc1022b9b71fbaadc62c2ed2c47f37
axpedro <pedruax@hotmail.com>
2024-02-13 22:02:39 -0300

    Atualizacao da calculadora html+js

--- Semana05/js2.js
+++ Semana05/js2.js
@@ -1,10 +1,40 @@
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
-let resultado;
-document.getElementById('somar') = somar(num1, num2);
 
-function somar(num1, num2) {
- resultado =  num1 + num2;
- return resultado
-}
-document.getElementById("resultado").innerText = resultado;
+
+let botaosomar = document.getElementById("somar");
+botaosomar.addEventListener('click',soma);
+function soma(){
+    let valorUm = parseInt(num1.value);
+    let valorDois= parseInt(num2.value);
+    let resultado =  valorUm + valorDois;
+    document.getElementById("resultado").value = resultado;
+    }
+
+
+    let botaoSubtrair = document.getElementById("subtrair");
+    botaoSubtrair.addEventListener('click',subtrair);
+    function subtrair(){
+        let valorUm = parseInt(num1.value);
+        let valorDois= parseInt(num2.value);
+        let resultado =  valorUm - valorDois;
+        document.getElementById("resultado").value = resultado;
+        }
+
+        let botaoDivdir = document.getElementById("dividir");
+        botaoDivdir.addEventListener('click',dividir);
+    function dividir(){
+        let valorUm = parseInt(num1.value);
+        let valorDois= parseInt(num2.value);
+        let resultado =  valorUm / valorDois;
+        document.getElementById("resultado").value = resultado;
+        }
+
+        let botaoMultiplicar = document.getElementById("multiplicar");
+        botaoMultiplicar.addEventListener('click',multiplicar);
+    function multiplicar(){
+        let valorUm = parseInt(num1.value);
+        let valorDois= parseInt(num2.value);
+        let resultado =  valorUm * valorDois;
+        document.getElementById("resultado").value = resultado;
+        }
