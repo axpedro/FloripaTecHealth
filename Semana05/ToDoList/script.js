bd55ac9891afcbae20ed6821f483feb8cf4cabee
axpedro <pedruax@hotmail.com>
2024-02-17 17:31:09 -0300

    Adicionado as logicas para verificação se item está na lista. Botao checked tachado. Estilização. Item adicionado na Lista.

--- Semana05/ToDoList/script.js
+++ Semana05/ToDoList/script.js
@@ -1,3 +1,44 @@
+var listaInputs = document.querySelectorAll('ul li span');
+var tarefaInput =document.getElementById('tarefa');
+var botaoInserir = document.getElementById('inserir');
+var lista = document.getElementById('lista');
 
-  
-  
+function checaLista (){
+var inputComTrim = tarefaInput.value.trim().toLowerCase();
+
+    for (var i = 0; i < listaInputs.length; i++) {
+    var elementosSpan = listaInputs[i].textContent.trim().toLowerCase();
+    if(inputComTrim === elementosSpan ){
+        alert('Você já possui uma tarefa adicionada com este nome!');
+        tarefaInput.value="";
+                return;
+        
+    } 
+    else if(inputComTrim == ""){
+        alert('Você não pode inserir um campo vazio');
+        tarefaInput.value="";
+        return;
+    }
+    
+}
+lista.innerHTML += "<li><input type=\"checkbox\" />" + 
+"<span>" + tarefaInput.value + "</span>" +
+"<button class=\"remove\"><i class=\"fa-solid fa-trash\"></i></button></li>";
+tarefaInput.value = "";
+}
+
+
+botaoInserir.addEventListener('click', function(event) {
+    checaLista();
+
+});
+
+
+
+
+
+
+
+
+
+
