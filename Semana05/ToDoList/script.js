c4109f3937da7c9bbff2ab745b8c362fa73e54ea
axpedro <pedruax@hotmail.com>
2024-02-21 20:01:01 -0300

    adicioando a funcao exlcuir do botao

--- Semana05/ToDoList/script.js
+++ Semana05/ToDoList/script.js
@@ -1,44 +1,52 @@
 var listaInputs = document.querySelectorAll('ul li span');
-var tarefaInput =document.getElementById('tarefa');
+var tarefaInput = document.getElementById('tarefa');
 var botaoInserir = document.getElementById('inserir');
 var lista = document.getElementById('lista');
+var botaoExcluir = document.getElementsByClassName('fa-solid fa-trash');
 
-function checaLista (){
-var inputComTrim = tarefaInput.value.trim().toLowerCase();
+function checaLista() {
+  var inputComTrim = tarefaInput.value.trim().toLowerCase();
 
-    for (var i = 0; i < listaInputs.length; i++) {
+  for (var i = 0; i < listaInputs.length; i++) {
     var elementosSpan = listaInputs[i].textContent.trim().toLowerCase();
-    if(inputComTrim === elementosSpan ){
-        alert('Você já possui uma tarefa adicionada com este nome!');
-        tarefaInput.value="";
-                return;
-        
-    } 
-    else if(inputComTrim == ""){
-        alert('Você não pode inserir um campo vazio');
-        tarefaInput.value="";
-        return;
+    if (inputComTrim === elementosSpan) {
+      alert('Você já possui uma tarefa adicionada com este nome!');
+      tarefaInput.value = '';
+      return;
+    } else if (inputComTrim == '') {
+      alert('Você não pode inserir um campo vazio');
+      tarefaInput.value = '';
+
+      return;
     }
-    
+  }
+  lista.innerHTML +=
+    '<li><input type="checkbox" />' +
+    '<span>' +
+    tarefaInput.value +
+    '</span>' +
+    '<button class="remove"><i class="fa-solid fa-trash"></i></button></li>';
+  tarefaInput.value = '';
+  updateDeleteButtonListener();
 }
-lista.innerHTML += "<li><input type=\"checkbox\" />" + 
-"<span>" + tarefaInput.value + "</span>" +
-"<button class=\"remove\"><i class=\"fa-solid fa-trash\"></i></button></li>";
-tarefaInput.value = "";
-}
 
-
-botaoInserir.addEventListener('click', function(event) {
-    checaLista();
-
+botaoInserir.addEventListener('click', function (event) {
+  checaLista();
 });
 
-
-
-
-
-
-
-
-
-
+function updateDeleteButtonListener() {
+  var botaoExcluir = document.querySelectorAll('.remove');
+  botaoExcluir.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+      var target = event.target;
+      if (target.classList.contains('remove')) {
+        // Find the closest <li> element and remove it
+        var listItem = target.closest('li');
+        if (listItem) {
+          listItem.remove();
+        }
+      }
+    });
+  });
+}
+updateDeleteButtonListener();
