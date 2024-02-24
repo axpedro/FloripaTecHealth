b71da829b20fbd05c49ee9e64861f14b7dca9184
axpedro <pedruax@hotmail.com>
2024-02-24 13:40:40 -0300

    Finalizando a atividade da semana com o contador de tarefas

--- Semana05/ToDoList/script.js
+++ Semana05/ToDoList/script.js
@@ -2,13 +2,60 @@ var listaInputs = document.querySelectorAll('ul li span');
 var tarefaInput = document.getElementById('tarefa');
 var botaoInserir = document.getElementById('inserir');
 var lista = document.getElementById('lista');
-//var botaoExcluir = document.getElementsByClassName('fa-solid fa-trash');
+var contador = 0;
+var numerador = document.getElementById('contador');
+
+
+
+
+function checaLista() {
+  var inputComTrim = tarefaInput.value.trim().toLowerCase();
+
+  if (inputComTrim == '') {
+    alert('Você não pode inserir um campo vazio');
+    tarefaInput.value = '';
+
+    return;
+  }
+   
+    for (var i = 0; i < listaInputs.length; i++) {
+      var elementosSpan = listaInputs[i].textContent.trim().toLowerCase();
+      if (inputComTrim === elementosSpan) {
+        alert('Você já possui uma tarefa adicionada com este nome!');
+        tarefaInput.value = '';
+        return;
+      } 
+
+
+  }
+  lista.innerHTML +=
+  '<li><input type="checkbox" />' +
+  '<span>' +
+  tarefaInput.value +
+  '</span>' +
+  '<button class="remove"><i class="fa-solid fa-trash"></i></button></li>';
+tarefaInput.value = '';
+contador +=1;
+numerador.innerHTML = contador;
+updateDeleteButtonListener();
+
+
+  }
+ 
+
+  
+
+  
+
+
+botaoInserir.addEventListener('click', function (event) {
+  checaLista();
+});
 
 
 lista.addEventListener('click', function (event) {
   var target = event.target;
   if (target.classList.contains('remove')) {
-
 
     var confirmaExcluir = confirm("Essa exlusão é permanente, deseja continuar?");
     // Find the closest <li> element and remove it
@@ -17,46 +64,14 @@ lista.addEventListener('click', function (event) {
       var listItem = target.closest('li');
     if (listItem) {
       listItem.remove();
+      contador -=1;
+  numerador.innerHTML = contador;
     }
     }
     
   }
 }
 );
-
-
-
-
-function checaLista() {
-  var inputComTrim = tarefaInput.value.trim().toLowerCase();
-
-  for (var i = 0; i < listaInputs.length; i++) {
-    var elementosSpan = listaInputs[i].textContent.trim().toLowerCase();
-    if (inputComTrim === elementosSpan) {
-      alert('Você já possui uma tarefa adicionada com este nome!');
-      tarefaInput.value = '';
-      return;
-    } else if (inputComTrim == '') {
-      alert('Você não pode inserir um campo vazio');
-      tarefaInput.value = '';
-
-      return;
-    }
-  }
-  lista.innerHTML +=
-    '<li><input type="checkbox" />' +
-    '<span>' +
-    tarefaInput.value +
-    '</span>' +
-    '<button class="remove"><i class="fa-solid fa-trash"></i></button></li>';
-  tarefaInput.value = '';
-  updateDeleteButtonListener();
-}
-
-botaoInserir.addEventListener('click', function (event) {
-  checaLista();
-});
-
 function updateDeleteButtonListener() {
   var botaoExcluir = document.querySelectorAll('.remove');
   botaoExcluir.forEach(function (button) {
