7fe8e7ceccb558338ce7859e8abc1514bccada6d
axpedro <pedruax@hotmail.com>
2024-03-01 19:54:07 -0300

    att

--- Semana05/ToDoList/script.js
+++ Semana05/ToDoList/script.js
@@ -5,69 +5,66 @@ var lista = document.getElementById('lista');
 var contador = 0;
 var numerador = document.getElementById('contador');
 
-
-
-
 function checaLista() {
+  var isDuplicate = false;
   var inputComTrim = tarefaInput.value.trim().toLowerCase();
-
-  if (inputComTrim == '') {
+  if (!inputComTrim) {
     alert('Você não pode inserir um campo vazio');
     tarefaInput.value = '';
-
     return;
   }
-   
-    for (var i = 0; i < listaInputs.length; i++) {
-      var elementosSpan = listaInputs[i].textContent.trim().toLowerCase();
-      if (inputComTrim === elementosSpan) {
-        alert('Você já possui uma tarefa adicionada com este nome!');
-        tarefaInput.value = '';
-        return;
-      } 
+  listaInputs = document.querySelectorAll('ul li span');
 
-
+  for (var i = 0; i < listaInputs.length; i++) {
+    var elementosSpan = listaInputs[i].textContent.trim().toLowerCase();
+    if (inputComTrim === elementosSpan) {
+      isDuplicate = true;
+      break;
+    }
   }
+  if (isDuplicate) {
+    alert('Você já possui uma tarefa adicionada com este nome!');
+    tarefaInput.value = '';
+    return;
+  }
+
   lista.innerHTML +=
-  '<li><input type="checkbox" />' +
-  '<span>' +
-  tarefaInput.value +
-  '</span>' +
-  '<button onclick="deletar(this)" class="remove"><i class="fa-solid fa-trash"></i></button></li>';
-tarefaInput.value = '';
-contador +=1;
-numerador.innerHTML = contador;
-updateDeleteButtonListener();
+    '<li><input type="checkbox" />' +
+    '<span>' +
+    tarefaInput.value +
+    '</span>' +
+    '<button onclick="deletar(this)" class="remove"><i class="fa-solid fa-trash"></i></button></li>';
+  tarefaInput.value = '';
+  contador += 1;
+  numerador.innerHTML = contador;
+}
 
-
-  }
-  botaoInserir.addEventListener('click', function (event) {
+botaoInserir.addEventListener('click', function (event) {
   checaLista();
 });
-function deletar(target){
+function deletar(target) {
   var listItem = target.closest('li');
   listItem.remove();
-  numerador.innerHTML = contador -=1;
+  numerador.innerHTML = contador -= 1;
 }
 
 lista.addEventListener('click', function (event) {
   var target = event.target;
   if (target.classList.contains('remove')) {
+    var confirmaExcluir = confirm(
+      'Essa exclusão é permanente, deseja continuar?'
+    );
 
-    var confirmaExcluir = confirm("Essa exlusão é permanente, deseja continuar?");
-    
-    if(confirmaExcluir){
+    if (confirmaExcluir) {
       var listItem = target.closest('li');
-    if (listItem) {
-      listItem.remove();
-      contador -=1;
-  numerador.innerHTML = contador;
+      if (listItem) {
+        listItem.remove();
+        contador -= 1;
+        numerador.innerHTML = contador;
+      }
     }
-    }
-    
   }
-}
-);
+});
 // function updateDeleteButtonListener() {
 //   var botaoExcluir = document.querySelectorAll('.remove');
 //   botaoExcluir.forEach(function (button) {
